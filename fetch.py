f8ba00c97c8ad0493811cc377f7e165b7cf09aaa
J Kendal <13680617+joekendal@users.noreply.github.com>
2019-07-20 09:48:36 +0100

    small changes

--- fetch.py
+++ fetch.py
@@ -1,11 +1,12 @@
 import json, requests, sqlalchemy
 from sqlalchemy import create_engine, Column, Integer, Float, String, Date, ForeignKey
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+
 
 engine = create_engine('sqlite:///:memory:', echo=True)
 Base = declarative_base()
-
+Session = sessionmaker(bind=engine)
 
 
 STOCKS = ['AAPL', 'TSLA', 'AMZN', 'FB', 'IBM', 'SNAP', 'NVDA', 'GOOGL']
@@ -17,6 +18,7 @@ URL = "https://financialmodelingprep.com/api/v3/financials/income-statement/"
 class Stock(Base):
     __tablename__ = "stocks"
     id = Column(String, primary_key=True) # Ticker Symbol
+    financials = relationship("AnnualFinancial", back_populates="stock")
 
     def __repr__(self):
         return "<Stock(symbol='%s')>" % self.id
@@ -25,6 +27,7 @@ class AnnualFinancial(Base):
     __tablename__ = "annualfinancials"
     id = Column(Integer, primary_key=True)
     stock_id = Column(String, ForeignKey('stocks.id'))
+    stock = relationship("Stock", back_populates="financials")
 
     date = Column(Date)
     revenue = Column(Float)
@@ -34,20 +37,30 @@ class AnnualFinancial(Base):
     def __repr__(self):
         return "<Financial(stock='%s',date='%s')>" % (self.stock_id, self.date)
 
-
-for stock in STOCKS:
-    print("fetching %s" % stock)
-    r = requests.get(URL+stock)
-    if r.status_code == 200:
-        new_stock = Stock(
-            id = stock,
-        )
-        financials = r.json()['financials']
-        for financial in financials:
-            new_financial = AnnualFinancial(
-                stock_id = new_stock.id,
-                date = financial['date'],
-                revenue = financial['Revenue'],
-                revenue_growth = financial['Revenue Growth']
-            )
-            print(new_financial)
+def fetch():
+    for stock in STOCKS:
+        print("fetching %s" % stock)
+        r = requests.get(URL+stock)
+        if r.status_code == 200:
+            session = Session()
+            new_stock = Stock(id=stock)
+            # Stock may already be in DB but check if all annual financials exist...
+            if not session.query(Stock).filter_by(id=stock).one_or_none():
+                session.add(new_stock)
+                session.commit()
+            financials = r.json()['financials']
+            # Assumed that financials are ordered most recent at first index
+            if not session.query(Stock).filter_by(date=financials[0]['date']).one_or_none():
+                for financial in financials:
+                    new_financial = AnnualFinancial(
+                        stock_id = new_stock.id,
+                        date = financial['date'],
+                        revenue = financial['Revenue'],
+                        revenue_growth = financial['Revenue Growth'],
+                        # ...
+                    )
+                    session.add(new_financial)
+                    session.commit()
+                    print(new_financial)
+        else:
+            print("[!] Error fetching %s" % stock)
