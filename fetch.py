bfb34ec65d8ad07d9fa5c254139a03317e36fef7
J Kendal <13680617+joekendal@users.noreply.github.com>
2019-07-24 05:03:38 +0100

    Added balance sheets and cash flow fetches

--- fetch.py
+++ fetch.py
@@ -1,130 +1,154 @@
-import json, requests, sqlalchemy, datetime
-from sqlalchemy import create_engine, Column, Integer, Float, String, Date, ForeignKey
-from sqlalchemy.orm import relationship, sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+import json, requests
+from sqlalchemy.orm import sessionmaker
+from datetime import datetime
+from models import *
 
 
-engine = create_engine('sqlite:///test.db', echo=True)
-Base = declarative_base()
+engine = create_engine('sqlite:///test.db')
+Base.metadata.create_all(engine, checkfirst=True)
 Session = sessionmaker(bind=engine)
 
 
 STOCKS = ['AAPL', 'TSLA', 'AMZN', 'FB', 'IBM', 'SNAP', 'NVDA', 'GOOGL']
 
-# Default returns JSON
-ANNUAL_INCOME_STATEMENT_URL = "https://financialmodelingprep.com/api/v3/financials/income-statement/"
+def get_financial_statements(stock, db):
+
+    statement_types = [
+        {
+            "URL": "https://financialmodelingprep.com/api/v3/financials/income-statement/",
+            "Model": AnnualIncomeStatement
+        },
+        {
+            "URL": "https://financialmodelingprep.com/api/v3/financials/balance-sheet-statement/",
+            "Model": AnnualBalanceSheet
+        },
+        {
+            "URL": "https://financialmodelingprep.com/api/v3/financials/cash-flow-statement/",
+            "Model": AnnualCashFlow
+        }
+    ]
+
+    for statement_type in statement_types:
+
+        r = requests.get(statement_type['URL']+stock)
+        if r.status_code == 200:
+            if not db.query(Stock).filter_by(id=stock).one_or_none():
+                new_stock = Stock(id=stock)
+                db.add(new_stock)
+                db.commit()
+
+            statements = r.json()['financials']
+
+            for statement in statements:
+                statement_date = datetime.strptime(statement['date'], '%Y-%m-%d').date()
+                if not db.query(statement_type['Model'])\
+                    .filter(statement_type['Model'].stock_id==stock,
+                            statement_type['Model'].date==statement_date).one_or_none():
+
+                    if statement_type['Model'] == AnnualIncomeStatement:
+                        new_statement = AnnualIncomeStatement(
+                            stock_id = stock,
+                            date = statement_date,
+                            revenue = statement['Revenue'],
+                            revenue_growth = statement['Revenue Growth'] or None,
+                            cost_of_revenue = statement['Cost of Revenue'],
+                            gross_profit = statement['Gross Profit'],
+                            r_and_d_expenses = statement['R&D Expenses'],
+                            sg_and_a_expense = statement['SG&A Expense'],
+                            operating_expenses = statement['Operating Expenses'],
+                            operating_income = statement['Operating Income'],
+                            interest_expense = statement['Interest Expense'],
+                            earnings_pre_tax = statement['Earnings before Tax'],
+                            income_tax_expense = statement['Income Tax Expense'],
+                            net_income_non_controlling = statement['Net Income - Non-Controlling int'],
+                            net_income_discontinued_ops = statement['Net Income - Discontinued ops'],
+                            net_income = statement['Net Income'],
+                            preferred_dividends = statement['Preferred Dividends'],
+                            net_income_com = statement['Net Income Com'],
+                            eps = statement['EPS'],
+                            eps_diluted = statement['EPS Diluted'],
+                            weighted_avg_shs_out = statement['Weighted Average Shs Out'] or None,
+                            weighted_avg_shs_out_dil = statement['Weighted Average Shs Out (Dil)'] or None,
+                            dividend_per_share = statement['Dividend per Share'],
+                            gross_margin = statement['Gross Margin'],
+                            ebitda_margin = statement['EBITDA Margin'],
+                            ebit_margin = statement['EBIT Margin'],
+                            profit_margin = statement['Profit Margin'],
+                            free_cash_flow_margin = statement['Free Cash Flow margin'],
+                            ebitda = statement['EBITDA'],
+                            ebit = statement['EBIT'],
+                            consolidated_income = statement['Consolidated Income'],
+                            earnings_pre_tax_margin = statement['Earnings Before Tax Margin'],
+                            net_profit_margin = statement['Net Profit Margin']
+                        )
+                    elif statement_type['Model'] == AnnualBalanceSheet:
+                        new_statement = AnnualBalanceSheet(
+                            stock_id = stock,
+                            date = statement_date,
+                            cash_and_equiv = statement['Cash and cash equivalents'],
+                            short_term_investments = statement['Short-term investments'],
+                            cash_and_short_term_investments = statement['Cash and short-term investments'],
+                            receivables = statement['Receivables'],
+                            inventories = statement['Inventories'],
+                            total_current_assets = statement['Total current assets'],
+                            property_plant_and_equipment_net = statement['Property, Plant & Equipment Net'],
+                            goodwill_and_intangible_assets = statement['Goodwill and Intangible Assets'],
+                            long_term_investments = statement['Long-term investments'],
+                            tax_assets = statement['Tax assets'],
+                            total_non_current_assets = statement['Total non-current assets'],
+                            total_assets = statement['Total assets'],
+                            payables = statement['Payables'],
+                            short_term_debt = statement['Short-term debt'],
+                            total_current_liabilities = statement['Total current liabilities'],
+                            long_term_debt = statement['Long-term debt'],
+                            total_debt = statement['Total debt'],
+                            deferred_revenue = statement['Deferred revenue'],
+                            tax_liabilities = statement['Tax Liabilities'],
+                            deposit_liabilities = statement['Deposit Liabilities'],
+                            total_non_current_liabilities = statement['Total non-current liabilities'],
+                            total_liabilities = statement['Total liabilities'],
+                            other_comprehensive_income = statement['Other comprehensive income'],
+                            retained_earnings_deficit = statement['Retained earnings (deficit)'],
+                            total_shareholders_equity = statement['Total shareholders equity'],
+                            investments = statement['Investments'],
+                            net_debt = statement['Net Debt'],
+                            other_assets = statement['Other Assets'],
+                            other_liabilities = statement['Other Liabilities']
+                        )
+
+                    elif statement_type['Model'] == AnnualCashFlow:
+                        new_statement = AnnualCashFlow(
+                            stock_id = stock,
+                            date = statement_date,
+                            depreciation_and_amortization = statement['Depreciation & Amortization'],
+                            stock_based_compensation = statement['Stock-based compensation'],
+                            operating_cash_flow = statement['Operating Cash Flow'],
+                            capital_expenditure = statement['Capital Expenditure'],
+                            acquisitions_and_disposals = statement['Acquisitions and disposals'],
+                            investment_purchases_and_sales = statement['Investment purchases and sales'],
+                            investing_cash_flow = statement['Investing Cash flow'],
+                            issuance_repayment_of_debt = statement['Issuance (repayment) of debt'],
+                            issuance_buybacks_of_shares = statement['Issuance (buybacks) of shares'],
+                            dividend_payments = statement['Dividend payments'],
+                            financing_cash_flow = statement['Financing Cash Flow'],
+                            effect_of_forex_changes_on_cash = statement['Effect of forex changes on cash'],
+                            net_cash_flow = statement['Net cash flow / Change in cash'],
+                            free_cash_flow = statement['Free Cash Flow'],
+                            net_cash = statement['Net Cash/Marketcap']
+                        )
+
+                    db.add(new_statement)
+                    db.commit()
+        else:
+            print("[!] Error fetching %s Income Statements" % stock)
 
 
-class Stock(Base):
-    __tablename__ = "stocks"
-    id = Column(String, primary_key=True) # Ticker Symbol
-    income_statements = relationship("AnnualIncomeStatement", back_populates="stock")
-
-    def __repr__(self):
-        return "<Stock(symbol='%s')>" % self.id
-
-class AnnualIncomeStatement(Base):
-    __tablename__ = "annualincomestatements"
-
-    # Composite key
-    stock_id = Column(String, ForeignKey('stocks.id'), primary_key=True)
-    date = Column(Date, primary_key=True)
-
-    stock = relationship("Stock", back_populates="income_statements")
-
-    revenue = Column(Float)
-    revenue_growth = Column(Float)
-    cost_of_revenue = Column(Float)
-    gross_profit = Column(Float)
-    r_and_d_expenses = Column(Float)
-    sg_and_a_expense = Column(Float)
-    operating_expenses = Column(Float)
-    operating_income = Column(Float)
-    interest_expense = Column(Float)
-    earnings_pre_tax = Column(Float)
-    income_tax_expense = Column(Float)
-    net_income_non_controlling = Column(Float)
-    net_income_discontinued_ops = Column(Float)
-    net_income = Column(Float)
-    preferred_dividends = Column(Float)
-    net_income_com = Column(Float)
-    eps = Column(Float)
-    eps_diluted = Column(Float)
-    weighted_avg_shs_out = Column(Float)
-    weighted_avg_shs_out_dil = Column(Float)
-    dividend_per_share = Column(Float)
-    gross_margin = Column(Float)
-    ebitda_margin = Column(Float)
-    ebit_margin = Column(Float)
-    profit_margin = Column(Float)
-    free_cash_flow_margin = Column(Float)
-    ebitda = Column(Float)
-    ebit = Column(Float)
-    consolidated_income = Column(Float)
-    earnings_pre_tax_margin = Column(Float)
-    net_profit_margin = Column(Float)
-
-    def __repr__(self):
-        return "<IncomeStatement(stock='%s',date='%s')>" % (self.stock_id, self.date)
 
 def fetch():
-    session = Session()
+    db = Session()
     for stock in STOCKS:
         print("[*] Fetching %s..." % stock)
-        r = requests.get(ANNUAL_INCOME_STATEMENT_URL+stock)
-        if r.status_code == 200:
-            if not session.query(Stock).filter_by(id=stock).one_or_none():
-                new_stock = Stock(id=stock)
-                session.add(new_stock)
-                session.commit()
-
-            income_statements = r.json()['financials']
-
-            for statement in income_statements:
-                statement_date = datetime.datetime.strptime(statement['date'], '%Y-%m-%d').date()
-                if not session.query(AnnualIncomeStatement)\
-                    .filter(AnnualIncomeStatement.stock_id==stock,
-                            AnnualIncomeStatement.date==statement_date).one_or_none():
-                    new_statement = AnnualIncomeStatement(
-                        stock_id = stock,
-                        date = statement_date,
-                        revenue = statement['Revenue'],
-                        revenue_growth = statement['Revenue Growth'] or None,
-                        cost_of_revenue = statement['Cost of Revenue'],
-                        gross_profit = statement['Gross Profit'],
-                        r_and_d_expenses = statement['R&D Expenses'],
-                        sg_and_a_expense = statement['SG&A Expense'],
-                        operating_expenses = statement['Operating Expenses'],
-                        operating_income = statement['Operating Income'],
-                        interest_expense = statement['Interest Expense'],
-                        earnings_pre_tax = statement['Earnings before Tax'],
-                        income_tax_expense = statement['Income Tax Expense'],
-                        net_income_non_controlling = statement['Net Income - Non-Controlling int'],
-                        net_income_discontinued_ops = statement['Net Income - Discontinued ops'],
-                        net_income = statement['Net Income'],
-                        preferred_dividends = statement['Preferred Dividends'],
-                        net_income_com = statement['Net Income Com'],
-                        eps = statement['EPS'],
-                        eps_diluted = statement['EPS Diluted'],
-                        weighted_avg_shs_out = statement['Weighted Average Shs Out'] or None,
-                        weighted_avg_shs_out_dil = statement['Weighted Average Shs Out (Dil)'] or None,
-                        dividend_per_share = statement['Dividend per Share'],
-                        gross_margin = statement['Gross Margin'],
-                        ebitda_margin = statement['EBITDA Margin'],
-                        ebit_margin = statement['EBIT Margin'],
-                        profit_margin = statement['Profit Margin'],
-                        free_cash_flow_margin = statement['Free Cash Flow margin'],
-                        ebitda = statement['EBITDA'],
-                        ebit = statement['EBIT'],
-                        consolidated_income = statement['Consolidated Income'],
-                        earnings_pre_tax_margin = statement['Earnings Before Tax Margin'],
-                        net_profit_margin = statement['Net Profit Margin']
-                    )
-                    session.add(new_statement)
-                    session.commit()
-        else:
-            print("[!] Error fetching %s" % stock)
-
+        get_financial_statements(stock, db)
 
 """
 To find financials for stock by year
---
+++ models.py
@@ -0,0 +1,130 @@
+from sqlalchemy import create_engine, Column, Float, String, Date, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+
+class Stock(Base):
+    __tablename__ = "stocks"
+    id = Column(String, primary_key=True) # Ticker Symbol
+    income_statements = relationship("AnnualIncomeStatement", back_populates="stock")
+    balance_sheets = relationship("AnnualBalanceSheet", back_populates="stock")
+    cash_flows = relationship("AnnualCashFlow", back_populates="stock")
+
+    def __repr__(self):
+        return "<Stock(symbol='%s')>" % self.id
+
+
+class AnnualIncomeStatement(Base):
+    __tablename__ = "annualincomestatements"
+
+    stock_id = Column(String, ForeignKey('stocks.id'), primary_key=True)
+    date = Column(Date, primary_key=True)
+
+    stock = relationship("Stock", back_populates="income_statements")
+
+    revenue = Column(Float)
+    revenue_growth = Column(Float)
+    cost_of_revenue = Column(Float)
+    gross_profit = Column(Float)
+    r_and_d_expenses = Column(Float)
+    sg_and_a_expense = Column(Float)
+    operating_expenses = Column(Float)
+    operating_income = Column(Float)
+    interest_expense = Column(Float)
+    earnings_pre_tax = Column(Float)
+    income_tax_expense = Column(Float)
+    net_income_non_controlling = Column(Float)
+    net_income_discontinued_ops = Column(Float)
+    net_income = Column(Float)
+    preferred_dividends = Column(Float)
+    net_income_com = Column(Float)
+    eps = Column(Float)
+    eps_diluted = Column(Float)
+    weighted_avg_shs_out = Column(Float)
+    weighted_avg_shs_out_dil = Column(Float)
+    dividend_per_share = Column(Float)
+    gross_margin = Column(Float)
+    ebitda_margin = Column(Float)
+    ebit_margin = Column(Float)
+    profit_margin = Column(Float)
+    free_cash_flow_margin = Column(Float)
+    ebitda = Column(Float)
+    ebit = Column(Float)
+    consolidated_income = Column(Float)
+    earnings_pre_tax_margin = Column(Float)
+    net_profit_margin = Column(Float)
+
+    def __repr__(self):
+        return "<IncomeStatement(stock='%s',date='%s')>" % (self.stock_id, self.date)
+
+
+class AnnualBalanceSheet(Base):
+    __tablename__ = "annualbalancesheets"
+
+    stock_id = Column(String, ForeignKey('stocks.id'), primary_key=True)
+    date = Column(Date, primary_key=True)
+
+    stock = relationship("Stock", back_populates="balance_sheets")
+
+    cash_and_equiv = Column(Float)
+    short_term_investments = Column(Float)
+    cash_and_short_term_investments = Column(Float)
+    receivables = Column(Float)
+    inventories = Column(Float)
+    total_current_assets = Column(Float)
+    property_plant_and_equipment_net = Column(Float)
+    goodwill_and_intangible_assets = Column(Float)
+    long_term_investments = Column(Float)
+    tax_assets = Column(Float)
+    total_non_current_assets = Column(Float)
+    total_assets = Column(Float)
+    payables = Column(Float)
+    short_term_debt = Column(Float)
+    total_current_liabilities = Column(Float)
+    long_term_debt = Column(Float)
+    total_debt = Column(Float)
+    deferred_revenue = Column(Float)
+    tax_liabilities = Column(Float)
+    deposit_liabilities = Column(Float)
+    total_non_current_liabilities = Column(Float)
+    total_liabilities = Column(Float)
+    other_comprehensive_income = Column(Float)
+    retained_earnings_deficit = Column(Float)
+    total_shareholders_equity = Column(Float)
+    investments = Column(Float)
+    net_debt = Column(Float)
+    other_assets = Column(Float)
+    other_liabilities = Column(Float)
+
+    def __repr__(self):
+        return "<BalanceSheet(stock='%s',date='%s')>" % (self.stock_id, self.date)
+
+
+class AnnualCashFlow(Base):
+    __tablename__ = "annualcashflows"
+
+    stock_id = Column(String, ForeignKey('stocks.id'), primary_key=True)
+    date = Column(Date, primary_key=True)
+
+    stock = relationship("Stock", back_populates="cash_flows")
+
+    depreciation_and_amortization = Column(Float)
+    stock_based_compensation = Column(Float)
+    operating_cash_flow = Column(Float)
+    capital_expenditure = Column(Float)
+    acquisitions_and_disposals = Column(Float)
+    investment_purchases_and_sales = Column(Float)
+    investing_cash_flow = Column(Float)
+    issuance_repayment_of_debt = Column(Float)
+    issuance_buybacks_of_shares = Column(Float)
+    dividend_payments = Column(Float)
+    financing_cash_flow = Column(Float)
+    effect_of_forex_changes_on_cash = Column(Float)
+    net_cash_flow = Column(Float)
+    free_cash_flow = Column(Float)
+    net_cash = Column(Float)
+
+    def __repr__(self):
+        return "<CashFlow(stock='%s',date='%s')>" % (self.stock_id, self.date)
