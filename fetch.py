10f922ebba942b94f84f9ab52bcda233227669c1
J Kendal <13680617+joekendal@users.noreply.github.com>
2019-07-24 01:40:52 +0100

    Finished annual income statement fetch & save to db

--- fetch.py
+++ fetch.py
@@ -1,10 +1,10 @@
-import json, requests, sqlalchemy
+import json, requests, sqlalchemy, datetime
 from sqlalchemy import create_engine, Column, Integer, Float, String, Date, ForeignKey
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 
-engine = create_engine('sqlite:///:memory:', echo=True)
+engine = create_engine('sqlite:///test.db', echo=True)
 Base = declarative_base()
 Session = sessionmaker(bind=engine)
 
@@ -12,55 +12,131 @@ Session = sessionmaker(bind=engine)
 STOCKS = ['AAPL', 'TSLA', 'AMZN', 'FB', 'IBM', 'SNAP', 'NVDA', 'GOOGL']
 
 # Default returns JSON
-URL = "https://financialmodelingprep.com/api/v3/financials/income-statement/"
+ANNUAL_INCOME_STATEMENT_URL = "https://financialmodelingprep.com/api/v3/financials/income-statement/"
 
 
 class Stock(Base):
     __tablename__ = "stocks"
     id = Column(String, primary_key=True) # Ticker Symbol
-    financials = relationship("AnnualFinancial", back_populates="stock")
+    income_statements = relationship("AnnualIncomeStatement", back_populates="stock")
 
     def __repr__(self):
         return "<Stock(symbol='%s')>" % self.id
 
-class AnnualFinancial(Base):
-    __tablename__ = "annualfinancials"
-    id = Column(Integer, primary_key=True)
-    stock_id = Column(String, ForeignKey('stocks.id'))
-    stock = relationship("Stock", back_populates="financials")
+class AnnualIncomeStatement(Base):
+    __tablename__ = "annualincomestatements"
 
-    date = Column(Date)
+    # Composite key
+    stock_id = Column(String, ForeignKey('stocks.id'), primary_key=True)
+    date = Column(Date, primary_key=True)
+
+    stock = relationship("Stock", back_populates="income_statements")
+
     revenue = Column(Float)
     revenue_growth = Column(Float)
     cost_of_revenue = Column(Float)
+    gross_profit = Column(Float)
+    r_and_d_expenses = Column(Float)
+    sg_and_a_expense = Column(Float)
+    operating_expenses = Column(Float)
+    operating_income = Column(Float)
+    interest_expense = Column(Float)
+    earnings_pre_tax = Column(Float)
+    income_tax_expense = Column(Float)
+    net_income_non_controlling = Column(Float)
+    net_income_discontinued_ops = Column(Float)
+    net_income = Column(Float)
+    preferred_dividends = Column(Float)
+    net_income_com = Column(Float)
+    eps = Column(Float)
+    eps_diluted = Column(Float)
+    weighted_avg_shs_out = Column(Float)
+    weighted_avg_shs_out_dil = Column(Float)
+    dividend_per_share = Column(Float)
+    gross_margin = Column(Float)
+    ebitda_margin = Column(Float)
+    ebit_margin = Column(Float)
+    profit_margin = Column(Float)
+    free_cash_flow_margin = Column(Float)
+    ebitda = Column(Float)
+    ebit = Column(Float)
+    consolidated_income = Column(Float)
+    earnings_pre_tax_margin = Column(Float)
+    net_profit_margin = Column(Float)
 
     def __repr__(self):
-        return "<Financial(stock='%s',date='%s')>" % (self.stock_id, self.date)
+        return "<IncomeStatement(stock='%s',date='%s')>" % (self.stock_id, self.date)
 
 def fetch():
+    session = Session()
     for stock in STOCKS:
-        print("fetching %s" % stock)
-        r = requests.get(URL+stock)
+        print("[*] Fetching %s..." % stock)
+        r = requests.get(ANNUAL_INCOME_STATEMENT_URL+stock)
         if r.status_code == 200:
-            session = Session()
-            new_stock = Stock(id=stock)
-            # Stock may already be in DB but check if all annual financials exist...
             if not session.query(Stock).filter_by(id=stock).one_or_none():
+                new_stock = Stock(id=stock)
                 session.add(new_stock)
                 session.commit()
-            financials = r.json()['financials']
-            # Assumed that financials are ordered most recent at first index
-            if not session.query(Stock).filter_by(date=financials[0]['date']).one_or_none():
-                for financial in financials:
-                    new_financial = AnnualFinancial(
-                        stock_id = new_stock.id,
-                        date = financial['date'],
-                        revenue = financial['Revenue'],
-                        revenue_growth = financial['Revenue Growth'],
-                        # ...
+
+            income_statements = r.json()['financials']
+
+            for statement in income_statements:
+                statement_date = datetime.datetime.strptime(statement['date'], '%Y-%m-%d').date()
+                if not session.query(AnnualIncomeStatement)\
+                    .filter(AnnualIncomeStatement.stock_id==stock,
+                            AnnualIncomeStatement.date==statement_date).one_or_none():
+                    new_statement = AnnualIncomeStatement(
+                        stock_id = stock,
+                        date = statement_date,
+                        revenue = statement['Revenue'],
+                        revenue_growth = statement['Revenue Growth'] or None,
+                        cost_of_revenue = statement['Cost of Revenue'],
+                        gross_profit = statement['Gross Profit'],
+                        r_and_d_expenses = statement['R&D Expenses'],
+                        sg_and_a_expense = statement['SG&A Expense'],
+                        operating_expenses = statement['Operating Expenses'],
+                        operating_income = statement['Operating Income'],
+                        interest_expense = statement['Interest Expense'],
+                        earnings_pre_tax = statement['Earnings before Tax'],
+                        income_tax_expense = statement['Income Tax Expense'],
+                        net_income_non_controlling = statement['Net Income - Non-Controlling int'],
+                        net_income_discontinued_ops = statement['Net Income - Discontinued ops'],
+                        net_income = statement['Net Income'],
+                        preferred_dividends = statement['Preferred Dividends'],
+                        net_income_com = statement['Net Income Com'],
+                        eps = statement['EPS'],
+                        eps_diluted = statement['EPS Diluted'],
+                        weighted_avg_shs_out = statement['Weighted Average Shs Out'] or None,
+                        weighted_avg_shs_out_dil = statement['Weighted Average Shs Out (Dil)'] or None,
+                        dividend_per_share = statement['Dividend per Share'],
+                        gross_margin = statement['Gross Margin'],
+                        ebitda_margin = statement['EBITDA Margin'],
+                        ebit_margin = statement['EBIT Margin'],
+                        profit_margin = statement['Profit Margin'],
+                        free_cash_flow_margin = statement['Free Cash Flow margin'],
+                        ebitda = statement['EBITDA'],
+                        ebit = statement['EBIT'],
+                        consolidated_income = statement['Consolidated Income'],
+                        earnings_pre_tax_margin = statement['Earnings Before Tax Margin'],
+                        net_profit_margin = statement['Net Profit Margin']
                     )
-                    session.add(new_financial)
+                    session.add(new_statement)
                     session.commit()
-                    print(new_financial)
         else:
             print("[!] Error fetching %s" % stock)
+
+
+"""
+To find financials for stock by year
+
+session.query(AnnualIncomeStatement).join(Stock)\
+        .filter(Stock.id=='AAPL')\
+        .filter(AnnualIncomeStatement.date.like("2018%"))\
+        .one_or_none()
+or...
+
+session.query(AnnualIncomeStatement)\
+    .filter(AnnualIncomeStatement.stock_id=='AAPL', AnnualIncomeStatement.date.like("2018%"))\
+    .one_or_none()
+
+"""
